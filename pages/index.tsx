de7d9d69d153522167a0bfc0f78f8a747d2a8045
jialinhuangblog <jia0.togepiii@gmail.com>
2022-05-13 21:06:33 +0800

    build & deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,19 @@
-import Link from 'next/link'
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      Hello World. latest nextjs and react
+    <div
+      css={css`
+        background: #eaeaea;
+      `}
+    >
+      <p>Hello World. latest nextjs and react, we are using typescript</p>
+      <p>using emotion to decorate</p>
       <Link href="/about">
         <a>About</a>
       </Link>
     </div>
-  )
+  );
 }
