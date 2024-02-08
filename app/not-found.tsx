cd1280a6ca99b6d09bf9b39d31ee68518a61322c
jialinhuangblog <jia0.togepiii@gmail.com>
2024-02-08 23:43:20 +0800

    build & deploy

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from 'next'
+import Layout from '@/components/Layout'
+import NotFoundIcon from '@/components/NotFoundIcon'
+
+const NotFound: NextPage = () => {
+  return <NotFoundIcon />
+}
+export default NotFound
